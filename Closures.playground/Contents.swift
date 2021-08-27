83e694cb9181f2dc7e80d66d944694708d7b9e21
IvanEvseenko <ivan.evseenko.2017@gmail.com>
2021-08-27 22:04:12 +0300

    Closures apgraded

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-let numbers = [1,4,3,5,6,3,4,7,10,33,20]
+let numbers = [11,4,3,5,6,3,50,7,10,33,20]
 
 
 func nonOdd(i: Int) -> Bool {
@@ -53,3 +53,87 @@ func myFilter(array: [Int], simpleFunc: (Int) -> Int) -> [Int] {
 
 let newArray = myFilter(array: numbers) {$0 * 2}
 newArray
+let newArray1 = myFilter(array: numbers) {$0 + 2}
+newArray1
+
+
+ ////////
+func simpleFunc(simpleClosure: () -> ()) {
+    simpleClosure()
+    for i in 1...10 {
+        print("Number \(i)")
+    }
+    simpleClosure()
+}
+simpleFunc {
+    print("Hi! I'm closure")
+}
+
+
+func maxNumber(array: [Int], myClosure: (Int, Int) -> Bool) -> Int {
+    var i = 0
+    for number in array {
+        if myClosure(number, i) {
+            i = number
+        }
+    }
+    print(i)
+    return i
+}
+let n = maxNumber(array: numbers) {$0 > $1}
+n
+
+func filter(array: [Int], myClosure: (Int?, Int) -> Bool) -> Int {
+    
+    var number : Int?
+    
+    for value in array {
+        if myClosure(number, value) {
+            number = value
+        }
+    }
+    return number!
+}
+
+let result1 = filter(array: numbers) {
+    $0 == nil || $1 > $0!
+}
+result1
+
+func filter1(array: [Int], myClosure: (Int, Int?) -> Bool) -> Int {
+    
+    var halper : Int?
+    
+    for digit in array {
+        if myClosure(digit, halper) {
+            halper = digit
+        }
+    }
+    return halper!
+}
+let result2 = filter1(array: numbers) {
+    if $1 == nil || $1! > $0 {
+        return true
+    }
+    return false
+}
+result2
+
+//////////////////////////////////////
+let stroka = "Hello! How are you? Today is August 26."
+var charaktersArray : [Character] = []
+for i in stroka {
+    charaktersArray.append(i)
+}
+
+
+///////////// sorted method apple's guide
+let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+
+func backward (_ s1: String, _ s2: String) -> Bool {
+    return s1 > s2
+}
+
+let sortedArray = names.sorted(by: backward)
+
+
